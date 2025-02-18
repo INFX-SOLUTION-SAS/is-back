fbdc147bc1f0bc81f8015f7f1a734c408daa8ba3
juancpinedam <juan.cpinedam@gmail.com>
2025-02-18 09:05:49 -0500

    cambio de puertos 3

--- src/routes/composter/activityRoutes.js
+++ src/routes/composter/activityRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import controller from '../../controllers/composter/ActivityController.js'
+import controller from '../../controllers/composter/activityController.js'
 import verifyToken from '../../middleware/authMiddleware.js';
 const router = express.Router()
 
